9625c9aed00c1c3673489b867e293b5b0ce9e92e
olt egor <olt.egor@gmail.com>
2016-07-24 19:29:33 +0300

    ngFileBtn

--- ngFileBtn.js
+++ ngFileBtn.js
@@ -1,3 +1,15 @@
 /**
  * Created by darkspring on 24.07.2016.
  */
+angular.module('ngFileBtn', [])
+    .directive('ngFileBtn', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var fileDialogId = attrs.ngFileBtn;
+                function handler(e) { $("#" + fileDialogId).trigger("click"); }
+                element.on("click", handler);
+                scope.$destroy(function(){ element.off("click", handler); });
+            }
+        }
+    });
